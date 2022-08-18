f016092df3f16bb72dd996ffc6386a8c41bb48cf
BHEEMASHANKAR HK <93082253+BHEEMASHANKARHRK@users.noreply.github.com>
2022-08-18 20:49:04 +0530

    Initial commit

---
+++ Online-Attendance-Sytem/app/src/test/java/co/in/nixlab/attendance_manager/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package co.in.nixlab.attendance_manager;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
